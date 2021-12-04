66017b2e30b8bf45ae4b1c8d9ee69b2d05dd36aa
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2021-12-05 00:43:49 +0100

    feat: form request creator
    (cherry picked from commit d39cd9ab4a0d61c2a8e851b1fb796d9186d2a25b)

---
+++ src/FormRequestCreator.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Kishieel\RequestUnit;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormRequestCreator extends FormRequest
+{
+    /** @var array */
+    protected $rules;
+
+    /** @var null|callable */
+    protected $authorize;
+
+    /**
+     * @param array $rules
+     * @param callable|null $authorize
+     */
+    public function __construct(array $rules = [], ?callable $authorize = null)
+    {
+        parent::__construct();
+
+        $this->rules = $rules;
+        $this->authorize = $authorize;
+    }
+
+    /**
+     * @return array
+     */
+    public function rules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return !is_callable($this->authorize) || ($this->authorize)();
+    }
+
+    /**
+     * @param array $rules
+     * @return \Kishieel\RequestUnit\FormRequestCreator
+     */
+    public function setRules(array $rules): self
+    {
+        $this->rules = $rules;
+
+        return $this;
+    }
+
+    /**
+     * @param callable $authorize
+     * @return \Kishieel\RequestUnit\FormRequestCreator
+     */
+    public function setAuthorize(callable $authorize): self
+    {
+        $this->authorize = $authorize;
+
+        return $this;
+    }
+}
